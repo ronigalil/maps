3095f97da434a374af4bfd2ea99a32f8bf2e4617
jbteden <edental15052006@ gmail.com>
2024-10-09 14:34:05 +0300

    add screen

--- Screen.py
+++ Screen.py
@@ -1,5 +1,26 @@
 import pygame
+import consts
 
 
 screen = pygame.display.set_mode(
         (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+
+
+
+pygame.init()
+size = (consts.WINDOW_HEIGHT, consts.WINDOW_WIDTH)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("ביתילי לחייל")
+finish = False
+#COLOR
+WHITE = (255, 255, 255)
+screen.fill(WHITE)
+pygame.display.flip()
+#RUN
+while not finish:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
+pygame.quit()
+#MOUSE
+mouse_point =pygame.mouse.get_pos()
