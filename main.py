872d5270fb0d900938056802d0ba2985466ab258
jbt <hadasruimy@gmail.com>
2024-10-09 14:22:05 +0300

    try out

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ houses_list = []
 def add_house(address, space_available, text):
     house = {'address': address, 'space_available': space_available, 'text': text}
     houses_list.append(house)
+
+print("hello")
