5136bd4b234370c0812f391a4a7ee81f66bf475e
Galitka <galitkafin@gmail.com>
2024-10-09 13:56:37 +0300

    h

---
+++ Screen.py
@@ -0,0 +1,5 @@
+import pygame
+
+
+screen = pygame.display.set_mode(
+        (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
---
+++ main.py
@@ -0,0 +1,6 @@
+houses_list = []
+
+
+def add_house(address, space_available, text):
+    house = {'address': address, 'space_available': space_available, 'text': text}
+    houses_list.append(house)
